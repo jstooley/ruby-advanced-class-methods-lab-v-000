42293145727f2533fb3fcf35eadfc3e394074c3c
Justin Tooley <jstooley823@gmail.com>
2017-09-11 08:19:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name
+    else
+      self.create_by_name(name)
+    end
   end
 
 end
