31b0daca43077550ec41880bd7cbca25a1a65d4a
Justin Tooley <jstooley823@gmail.com>
2017-09-11 07:45:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,20 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    name = Song.create
+  end
+
+  def create_by_name(name)
+    song = self.new
+    song.name = name
+    self.class.all << song
+  end
+
 end
