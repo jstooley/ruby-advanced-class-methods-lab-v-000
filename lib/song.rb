2d9a30a3444ac0627260116174e313a50c9513c3
Justin Tooley <jstooley823@gmail.com>
2017-09-11 08:36:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,24 +39,24 @@ class Song
   def self.find_or_create_by_name(name)
      self.find_by_name(name) || self.create_by_name(name)
   end
-   
+
   def self.alphabetical
       @@all.sort_by{|song| song.name}
   end
 
-      def self.new_from_filename(filename)
+  def self.new_from_filename(filename)
 
-        row = filename
+      row = filename
 
-          data = row.split(" - ")
-          artist_name = data[0]
-          song_name = data[1].gsub(".mp3", "")
+      data = row.split(" - ")
+      artist_name = data[0]
+      song_name = data[1].gsub(".mp3", "")
 
-        song = self.new
-        song.name = song_name
-        song.artist_name = artist_name
-        song
-    end
+      song = self.new
+      song.name = song_name
+      song.artist_name = artist_name
+      song
+  end
 
 
 
