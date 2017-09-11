68c6bdd9b026250c255f78e34c165ce2cd44ce43
Justin Tooley <jstooley823@gmail.com>
2017-09-11 09:05:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-     self.find_by_name(name) || self.create_by_name(name)
+     if find_by_name(name) == nil
+        self.create_by_name(name)
+      else
+        find_by_name(name)
+      end
   end
 
   def self.alphabetical
